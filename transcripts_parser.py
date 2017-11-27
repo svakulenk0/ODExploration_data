adb7d6fe926d78302512ab18904c6172b884385a
vendi <svitlana.vakulenko@gmail.com>
2017-11-27 17:14:50 +0100

    new: parse all transcripts into a csv log for ProM

--- transcripts_parser.py
+++ transcripts_parser.py
@@ -16,7 +16,7 @@ import os
 
 SAMPLE_TRANSCRIPT_FILE = 'data_gv_at/transcript1_1.txt'
 # SAMPLE_TRANSCRIPT_FILE = 'opendataportal_at/transcript13_2.txt'
-
+DIRS =['data_gv_at', 'opendataportal_at']
 OPERATIONS = ['greeting()', 'set(keywords)', 'prompt(keywords)',
               'list(keywords)', 'success()', 'link(dataset)',
               'bool(data)', 'question(data)', 'top(keywords)', 'count(data)',
@@ -43,17 +43,37 @@ def test_parse_transcript():
     parse_transcript(SAMPLE_TRANSCRIPT_FILE)
 
 
-def parse_all_transcripts(dirs =['data_gv_at', 'opendataportal_at']):
+def parse_all_transcripts(dirs=DIRS):
     for portal_dir in dirs:
         for file in os.listdir(portal_dir):
-          if file[-4:] == '.txt':
-            # print file
-            parse_transcript('/'.join([portal_dir, file]))
-            print '\n'
+            if file[-4:] == '.txt':
+                # print file
+                parse_transcript('/'.join([portal_dir, file]))
+                print '\n'
 
+
+def produce_single_log(dirs=DIRS):
+    for portal_dir in dirs:
+        index = 1
+        for file_name in os.listdir(portal_dir):
+            if file_name[-4:] == '.txt':
+                with open('/'.join([portal_dir, file_name])) as file:
+                    for line in file:
+                        try:
+                            intent, turn, message = line.split('>')
+                            # show the process template
+                            print ';'.join([str(index), turn, intent])
+                            
+                            if intent not in OPERATIONS:
+                               print("Unknown intent" + intent)
+                        except:
+                            if '>' in line:
+                                print("Error parsing line:")
+                                print line
+                index += 1
 
 def main():
-    parse_all_transcripts()
+    produce_single_log()
 
 
 if __name__ == '__main__':
