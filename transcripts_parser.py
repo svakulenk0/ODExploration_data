81e58b202080cb110a085694c7b4f0c44e1ad712
vendi <svitlana.vakulenko@gmail.com>
2017-12-12 15:42:32 +0100

    new: cluster utterances by intents and count number of concepts per utterance

--- transcripts_parser.py
+++ transcripts_parser.py
@@ -23,13 +23,22 @@ OPERATIONS = ['greeting()', 'set(keywords)', 'prompt(keywords)',
               'verify()', 'reject()', 'confirm()', 'prompt(link)', 'more()']
 
 
-def parse_transcript(transcript_file):
+def parse_transcript(transcript_file, search_intent=None, max_n_vars=None):
     with open(transcript_file) as file:
         for line in file:
             try:
                 intent, turn, message = line.split('>')
-                # show the process template
-                print turn, intent
+                if search_intent:
+                    if intent == search_intent:
+                        print message.strip()
+                elif max_n_vars:
+                        vars_count = message.count("[[")
+                        print message.strip(), vars_count
+                        if vars_count > max_n_vars:
+                            max_n_vars = vars_count
+                else:
+                    # show the process template
+                    print turn, intent
                 
                 if intent not in OPERATIONS:
                    print("Unknown intent" + intent)
@@ -37,6 +46,7 @@ def parse_transcript(transcript_file):
                 if '>' in line:
                     print("Error parsing line:")
                     print line
+    return max_n_vars
 
 
 def test_parse_transcript():
@@ -49,7 +59,28 @@ def parse_all_transcripts(dirs=DIRS):
             if file[-4:] == '.txt':
                 # print file
                 parse_transcript('/'.join([portal_dir, file]))
-                print '\n'
+                # print '\n'
+
+
+def search_all_transcripts(intent='greeting()', dirs=DIRS):
+    for portal_dir in dirs:
+        for file in os.listdir(portal_dir):
+            if file[-4:] == '.txt':
+                # print file
+                parse_transcript('/'.join([portal_dir, file]), intent)
+                # print '\n'
+
+
+def count_vars(dirs=DIRS):
+    n_vars = [1]
+    for portal_dir in dirs:
+        for file in os.listdir(portal_dir):
+            if file[-4:] == '.txt':
+                # print file
+                n_vars.append(parse_transcript('/'.join([portal_dir, file]), max_n_vars=1))
+    print "\n"
+    print "Average number of variables per utterance", reduce(lambda x, y: x + y, n_vars) / len(n_vars)
+    print "Maximum number of variables per utterance", max(n_vars)
 
 
 def produce_single_log(dirs=DIRS):
@@ -72,8 +103,11 @@ def produce_single_log(dirs=DIRS):
                                 print line
                 index += 1
 
+
 def main():
-    produce_single_log()
+    # search_all_transcripts(intent='list(keywords)')
+    # parse_all_transcripts()
+    count_vars()
 
 
 if __name__ == '__main__':
