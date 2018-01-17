c7ebb68b49f862f529e1a4769d39bbf5aa36046a
vendi <svitlana.vakulenko@gmail.com>
2018-01-17 10:28:04 +0100

    new: count turns

--- transcripts_parser.py
+++ transcripts_parser.py
@@ -29,6 +29,7 @@ def parse_transcript(transcript_file, search_intent=None, max_n_vars=None):
     # parse concept type annotations
     concept_type = re.compile(']](.\*)')
     vars_counts = []
+    turns_count = 0
     with open(transcript_file) as file:
         for line in file:
             try:
@@ -37,14 +38,19 @@ def parse_transcript(transcript_file, search_intent=None, max_n_vars=None):
                     if intent == search_intent:
                         print message.strip()
                 elif max_n_vars:
+                        print turn, message.strip()
                         types = re.findall(concept_type, message)
                         concepts = [ctype.strip('*') for ctype in types if ctype not in ['H*', 'G*', '+*', '-*']]
+                        # concepts = [ctype.strip('*') for ctype in types if ctype not in ['H*']]
                         if concepts and turn == 'A':
+                        # if types and turn == 'A':
+                            turns_count += 1
                             vars_count = len(concepts)
+                            print "Variables:", vars_count
                             vars_counts.append(vars_count)
-                            print turn, message.strip()
-                            print concepts, vars_count
-                            print '\n'
+                            # print turn, message.strip()
+                            # print concepts, vars_count
+                            # print '\n'
                             # print turn, intent, message.strip(), types, vars_count
                         # else:
                         #     print turn, intent, message.strip()
@@ -58,7 +64,8 @@ def parse_transcript(transcript_file, search_intent=None, max_n_vars=None):
                 if '>' in line:
                     print("Error parsing line:")
                     print line
-    return vars_counts
+    print "Number of turns:", turns_count
+    return vars_counts, turns_count
 
 
 def test_parse_transcript():
@@ -87,17 +94,26 @@ def count_vars(dirs=DIRS):
     '''
     count the number of concept annotations per utterance
     '''
-    n_vars = [1]
+    n_vars = []
+    n_turns = []
     for portal_dir in dirs:
         for file in os.listdir(portal_dir):
             if file[-4:] == '.txt':
                 # print file
-                n_vars.extend(parse_transcript('/'.join([portal_dir, file]), max_n_vars=1))
+                nvars, nlines = parse_transcript('/'.join([portal_dir, file]), max_n_vars=1)
+                n_vars.extend(nvars)
+                n_turns.append(nlines)
                 print "\n"
     print "\n"
-    # print n_vars
+    print sorted(n_vars)
     print "Average number of variables per utterance", reduce(lambda x, y: x + y, n_vars) / len(n_vars)
     print "Maximum number of variables per utterance", max(n_vars)
+
+    print sorted(n_turns)
+    print n_turns
+    print "Average number of turns per dialog", reduce(lambda x, y: x + y, n_turns) / len(n_turns)
+    print "Minimum number of turns per dialog", min(n_turns)
+    print "Maximum number of turns per dialog", max(n_turns)
 
 
 def produce_single_log(dirs=DIRS):
